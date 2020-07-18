09b510f65ca7b3ebcc50373c840a074850d333e0
dsenam <dsena.m@hotmail.com>
2020-07-18 16:30:34 -0300

    Criação Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import {HeadContainer, HeadContent} from './styles'
+
+
+export default function Header() {
+    return (
+        <>
+            <HeadContainer>
+                <HeadContent>
+                    <h1>Product Hunt</h1>
+                </HeadContent>  
+            </HeadContainer>
+        </>
+    )
+}
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../../components/Header'
 
 export default function Main() {
     return (
         <>
-        <h1>Main</h1>
+            <Header/>
         </>
     )
 }
